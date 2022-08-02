aae79c957dfc06da3340320f98762def08c4ff43
HuyHa <huyha14082012@gmail.com>
2022-08-02 09:25:38 +0700

    markdown

--- src/features/markdown/Markdown.tsx
+++ src/features/markdown/Markdown.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
-
 import { usePageStore } from '~/stores/page'
+import { BlockType } from '~/utils/types'
 
 export const Markdown = () => {
-  const { title } = usePageStore()
-  return <div>{title}</div>
+  const { blocks } = usePageStore()
+
+  const markdown = blocks.map((block) => {
+    if (block.type === BlockType.TEXT) {
+      return {
+        type: BlockType.TEXT,
+        details: {
+          value: (block.details?.value as string)
+            .replaceAll('<p>', '')
+            .replaceAll('</p>', '')
+            .replaceAll('<strong>', '**')
+            .replaceAll('</strong>', '**')
+            .replaceAll('<em>', '*')
+            .replaceAll('</em>', '*')
+            .replaceAll(/\<br.*?\>/g, ''),
+        },
+      }
+    } else {
+      return block
+    }
+  })
+  return (
+    <pre className="whitespace-pre-wrap p-10">
+      {JSON.stringify(markdown, null, 2)}
+    </pre>
+  )
 }
